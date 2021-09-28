28e70267fd8e46dbdb8936e313c9d1b45b38c188
Ben Wilson <benjaminw1030@gmail.com>
2021-09-28 14:55:45 -0700

    create basic program

---
+++ JobBoard/Controllers/JobOpeningController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using JobBoard.Models;
+using System.Collections.Generic;
+
+namespace JobBoard.Controllers
+{
+  public class JobOpeningController : Controller
+  {
+
+    [HttpGet("/jobs")]
+    public ActionResult Index()
+    {
+      List<JobOpening> allJobs = JobOpening.GetAll();
+      return View(allJobs);
+    }
+
+    [HttpGet("/jobs/new")]
+    public ActionResult CreateJob()
+    {
+      return View();
+    }
+
+    [HttpPost("/jobs")]
+    public ActionResult Create(string title, string description, string name, string email, string phoneNumber)
+    {
+      ContactInfo myContactInfo = new ContactInfo(name, email, phoneNumber)
+      JobOpening myJob = new JobOpening(title, description, myContactInfo);
+      return RedirectToAction("Index");
+    }
+  }
+}
